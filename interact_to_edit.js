260bcf07efdfd8aa7b9ebacb9d35a2b4e9d4fd0c
Amir Hameed <me@amirhameed.com>
2013-10-09 20:38:46 +0500

    Little House Keeping

--- interact_to_edit.js
+++ interact_to_edit.js
@@ -1,68 +1,53 @@
 
-	$.fn.interact_to_edit = function(options)
-	{
-		var settings = $.extend
-		(
+$.fn.interact_to_edit = function(options)
+{
+	var settings = $.extend
+	(
+		{
+			target		: 	"td",
+			eventtype 	: 	"dblclick"
+		}, 	options
+	);
+	
+	$.menu 		= new Object();
+	$.menu.i 	= new Object();
+	$.menu.i.selected_node ;
+	$(settings.target).on( settings.eventtype , function()
+	{	
+		if( $( "td.selected" ).size() > 0 )
+		{
+			var new_value = $($.menu.i.selected_node).find("input.edit-frame").val();
+			$($.menu.i.selected_node).find("input.edit-frame").remove();
+		   	$($.menu.i.selected_node).text(new_value);
+		   	$($.menu.i.selected_node).removeClass("selected")
+		}
+		if($("td.selected").size() == 0)
+		{
+		$(this).addClass("selected");
+		$.menu.i.selected_node = $(this);
+		var selected_node_value = $(this).text();
+		$(this).text("");
+		$(this).append("<input type='text' class='edit-frame' value='"+selected_node_value+"'>")
+		$(this).find("input.edit-frame").focus();
+		$(this).find("inpu.edit-frame").select();
+		$(this).find("input.edit-frame").keypress(function (e) 
+		 { 
+			if (e.which == 13) 
 			{
-				target :"td",
-				eventtype : "dblclick"
-			}, options
-		);
-		
-		$.menu = new Object();
-		$.menu.i = new Object();
-		$.menu.i.selected_node ;
-		var selected = "none";
-		
-
-		$(settings.target).on( settings.eventtype , function()
-		{	
-			
-			
-			if($("td.selected").size() > 0)
-			{ //if something is already selected
-				
-				var new_value = $($.menu.i.selected_node).find("input.edit-frame").val();
-				
-				$($.menu.i.selected_node).find("input.edit-frame").remove();
-			   	$($.menu.i.selected_node).text(new_value);
-			   	$($.menu.i.selected_node).removeClass("selected")
-
-
+		   		var new_value = $(this).val();
+		   		// console.log("new value :" + new_value);
+		   		$(this).remove();
+		   		$($.menu.i.selected_node).text(new_value);
+		    	return false;    
 			}
+		 });
+		}
+	});
+	
 
-			if($("td.selected").size() == 0)
-			{
-			$(this).addClass("selected");
-			$.menu.i.selected_node = $(this);
-			var selected_node_value = $(this).text();
-
-			// console.log("Value of selected node captured :  "+ selected_node_value);
-			$(this).text("");
-			$(this).append("<input type='text' class='edit-frame' value='"+selected_node_value+"'>")
-			$(this).find("input.edit-frame").focus();
-			$(this).find("inpu.edit-frame").select();
-
-
-			$(this).find("input.edit-frame").keypress(function (e) 
-			 { 
-			 	
-				if (e.which == 13) 
-				{
-			   		var new_value = $(this).val();
-			   		// console.log("new value :" + new_value);
-			   		$(this).remove();
-			   		$($.menu.i.selected_node).text(new_value);
-			    	return false;    
-				}
-			 });
-			}
-		});
-		
-
-		
+	
 
 
 
-		return false;
-	};
+	return false;
+};
