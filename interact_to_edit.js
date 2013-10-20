19cfaa2462e371282bb95db93c29f8f0f241bbb8
Amir Hameed <me@amirhameed.com>
2013-10-20 22:27:03 +0500

    revert previous values on Escape & on Blur
    Added feature: It reverts previous value of the Target element on Escape
    Key Press and on Losing Focus

--- interact_to_edit.js
+++ interact_to_edit.js
@@ -12,8 +12,12 @@ $.fn.interact_to_edit = function(options)
 	$.menu 		= new Object();
 	$.menu.i 	= new Object();
 	$.menu.i.selected_node ;
+	$.menu.i.current_value ;
 	$(settings.target).on( settings.eventtype , function()
 	{	
+
+		//If a cell is already selected, it reverts the value to the
+		//the target element and loses focus.Removes class
 		if( $( "td.selected" ).size() > 0 )
 		{
 			var new_value = $($.menu.i.selected_node).find("input.edit-frame").val();
@@ -21,13 +25,14 @@ $.fn.interact_to_edit = function(options)
 		   	$($.menu.i.selected_node).text(new_value);
 		   	$($.menu.i.selected_node).removeClass("selected")
 		}
+
 		if($("td.selected").size() == 0)
 		{
 		$(this).addClass("selected");
 		$.menu.i.selected_node = $(this);
-		var selected_node_value = $(this).text();
+		var current_value = $(this).text();
 		$(this).text("");
-		$(this).append("<input type='text' class='edit-frame' value='"+selected_node_value+"'>")
+		$(this).append("<input type='text' class='edit-frame' value='"+current_value+"'>")
 		$(this).find("input.edit-frame").focus();
 		$(this).find("inpu.edit-frame").select();
 		$(this).find("input.edit-frame").keypress(function (e) 
@@ -41,6 +46,16 @@ $.fn.interact_to_edit = function(options)
 		    	return false;    
 			}
 		 });
+		$(this).find("input.edit-frame").focusout(function (e) 
+		 { 
+		   		var new_value = $($.menu.i.selected_node).find("input.edit-frame").val();
+				$($.menu.i.selected_node).find("input.edit-frame").remove();
+			   	$($.menu.i.selected_node).text(new_value);
+			   	$($.menu.i.selected_node).removeClass("selected")  
+			
+		 });
+
+
 		}
 	});
 	
